60c02cef1d8f79a8b5cbd3c3f7848a4abc7f1e98
Rory <jyljensen@outlook.com>
2025-12-20 20:19:38 +0800

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-
 import { cloudflare } from "@cloudflare/vite-plugin";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), cloudflare()],
-})
+  plugins: [react(), cloudflare(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
